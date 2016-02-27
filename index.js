a9087139b7b9f179e83ffd726ce634d64cca0ff8
Clement Bourgoin <cb@nokto.net>
2016-02-27 18:56:32 +0100

    Add basic Book model and book list

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-"use strict";
+'use strict';
 
 const app = require('express')();
+const mongoose = require('mongoose');
 
 const port = 8080;
 
+// Mongo
+mongoose.connect('mongodb://localhost/biblys');
+
+var Book = mongoose.model('Book', {
+  title: String,
+  ean: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: Date,
+  deletedAt: Date,
+});
+
 app.get('/', function(req, res) {
-  res.send('Hello, data!');
+  Book.find({}, function(err, books) {
+    books = books.map(function(book) {
+      return `<li>${book.title} (${book.ean})</li>`;
+    });
+    res.send(`<ul>${books.join()}</ul>`);
+  });
 });
 
 app.listen(port);
