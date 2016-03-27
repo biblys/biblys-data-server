4d941af99b48fe0c6d3d5427a40bd75faace5ac7
Clement Bourgoin <cb@nokto.net>
2016-03-27 22:00:06 +0200

    Add a isAdmin property to User model (+ tests)

--- models/user.js
+++ models/user.js
@@ -10,6 +10,10 @@ const UserSchema = new mongoose.Schema({
       return gk.generateKey(32);
     }
   },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
   createdAt: {
     type: Date,
     default: Date.now
