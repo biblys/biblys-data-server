a636c57ce7dfd919cc5e13c229b624c2e1a34b52
Clement <clement@actialuna.com>
2016-04-14 13:29:03 +0200

    Display 10 last added books on home page

--- public/bundle.js
+++ public/bundle.js
@@ -48,6 +48,21 @@
 
 	__webpack_require__(1);
 
+	document.addEventListener('DOMContentLoaded', function() {
+	  fetch('/api/v0/books/')
+	  .then(function(response) {
+	    return response.json();
+	  }).then(function(body) {
+	    const results = body.results;
+	    const booksUl = document.querySelector('#books');
+	    results.forEach(function(book) {
+	      const li = document.createElement('li');
+	      li.innerHTML = `<a href="/api/v0/books/${book.ean}">${book.title}</a>`;
+	      booksUl.appendChild(li);
+	    });
+	  });
+	});
+
 
 /***/ },
 /* 1 */
